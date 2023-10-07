176420670bdb3eaee03fb3a50f8ee08fe9cb591d
NicholasSumanto <93309312+NicholasSumanto@users.noreply.github.com>
2023-10-08 01:22:20 +0700

    Update seeding.js

--- Homework_8/App/Databases/seeding.js
+++ Homework_8/App/Databases/seeding.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { connectToDatabase } = require('../connect.js'); // Sesuaikan dengan path yang benar
+const { connectToDatabase } = require('../connect.js'); 
 
 // Baca isi file seeding.sql
 const seedQuery = fs.readFileSync('Databases/seeding.sql',{encoding: 'utf8'});
@@ -7,10 +7,10 @@ const seedQuery = fs.readFileSync('Databases/seeding.sql',{encoding: 'utf8'});
 // Fungsi untuk menjalankan perintah SQL seeding
 const seedDatabase = async () => {
   try {
-    const client = await connectToDatabase(); // Gunakan fungsi koneksi yang sudah ada
+    const client = await connectToDatabase(); 
     await client.query(seedQuery); // Ganti sqlScript menjadi seedQuery
     console.log('Seeding berhasil.');
-    client.end(); // Tutup koneksi setelah selesai
+    client.end(); 
   } catch (error) {
     console.error('Error seeding database:', error);
   }
